e42922b8bd3c3d6572571b37df914e6b72954b94
Andreas Hofmann <mail@andreas-hofmann.org>
2020-05-25 11:27:52 +0200

    helpers.js: Update durationHours to display hh:mm instead of hours as decimal.
    Signed-off-by: Andreas Hofmann <mail@andreas-hofmann.org>

--- src/helpers.js
+++ src/helpers.js
@@ -5,12 +5,37 @@ function secToHours(val) {
   return (value / 3600).toFixed(1);
 }
 
+function secToMinutes(val) {
+  var value = parseFloat(val);
+  return (value / 60).toFixed(1);
+}
+
 function durationSecs(from, to) {
-  return (moment(to)-moment(from))/1000;
+  let ms = parseFloat(moment(to)-moment(from));
+
+  if (isNaN(ms))
+    return 0;
+
+  return ms/1000;
+}
+
+function padLeading(str, len, pad) {
+  let s = String(str);
+  while (s.length < len) {
+    s = String(pad) + s;
+  }
+  return s;
 }
 
 function durationHours(from, to) {
-  return secToHours(durationSecs(from, to));
+  let secs = durationSecs(from, to);
+  let hours = secToHours(secs);
+  let minutes = parseFloat(secToMinutes(secs));
+
+  if (!minutes)
+    return "";
+
+  return String(Math.floor(hours)) + ":" + padLeading(Math.floor(minutes % 60), 2, 0);
 }
 
 function localdate(utc) {

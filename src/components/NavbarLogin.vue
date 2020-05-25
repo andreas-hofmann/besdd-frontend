6da9aa55ac96527355a54e435d6310eb39029ebd
Andreas Hofmann <mail@andreas-hofmann.org>
2020-05-25 08:30:34 +0200

    NavbarLogin.vue: Remove form, use links to plain django login/register pages.
    Signed-off-by: Andreas Hofmann <mail@andreas-hofmann.org>

--- src/components/NavbarLogin.vue
+++ src/components/NavbarLogin.vue
@@ -7,33 +7,9 @@
 
       <b-collapse is-nav id="nav-collapse">
         <b-navbar-nav class="ml-auto">
-          <b-nav-item-dropdown right text="Login">
-            <b-dropdown-form>
-                <b-form-group label="username" label-for="dropdown-form-user" @submit.stop.prevent>
-                <b-form-input
-                    id="dropdown-form-user"
-                    size="sm"
-                    v-model="username"
-                    placeholder="username"
-                ></b-form-input>
-                </b-form-group>
-
-                <b-form-group label="password" label-for="dropdown-form-password">
-                <b-form-input
-                    id="dropdown-form-password"
-                    type="password"
-                    size="sm"
-                    v-model="password"
-                    placeholder="password"
-                ></b-form-input>
-                </b-form-group>
-                <b-button variant="primary" size="sm" @click="tryLogin">Sign In</b-button>
-            </b-dropdown-form>
-            <b-dropdown-divider></b-dropdown-divider>
-            <b-dropdown-item-button>Sign up</b-dropdown-item-button>
-            <b-dropdown-divider></b-dropdown-divider>
-            <b-dropdown-item :to="{ name: 'About' }"> about </b-dropdown-item>
-          </b-nav-item-dropdown>
+          <b-nav-item href="/accounts/login/">Log in</b-nav-item>
+          <b-nav-item href="/accounts/register/">Sign up</b-nav-item>
+          <b-nav-item :to="{ name: 'About' }"> about </b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>

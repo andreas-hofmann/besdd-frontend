2519447285325d83df7f78cf788c061549f39be7
Andreas Hofmann <mail@andreas-hofmann.org>
2020-05-25 09:01:16 +0200

    Details.vue: Set default date range to 7 days.
    Signed-off-by: Andreas Hofmann <mail@andreas-hofmann.org>

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -73,7 +73,7 @@ export default {
 
   data: function() {
     return {
-      dateRangeDays: 14,
+      dateRangeDays: 7,
       dates: {
         from: moment().format("YYYY-MM-DD"),
         to: moment().format("YYYY-MM-DD"),
